59ce45435b1485480980ec404e0b9670ffb77026
Evgeny Norkin <norkinzheny@gmail.com>
2023-06-19 21:46:01 +0300

    first commit

---
+++ Desktop/Charp/Seminar_02/task10/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("ВВедите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNumber = (number/10)%10;
+System.Console.WriteLine($"Вторая цифра числа: {sumNumber}");
---
+++ Desktop/Charp/Seminar_02/task15/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру, обозначающую день недели (1-7): ");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+bool isWeekend = false;
+switch (dayOfWeek)
+{
+case 6: // Суббота
+case 7: // Воскресенье
+isWeekend = true;
+   break;
+            default:
+if (dayOfWeek >= 8 || dayOfWeek <=0){
+    Console.WriteLine("Вы вышли за предел дней недели (1-7), повторите попытку");
+    return;
+}
+break;
+}
+if (isWeekend)
+{
+Console.WriteLine("Этот день является выходным.");
+}
+else
+{
+Console.WriteLine("Этот день не является выходным.");
+}
