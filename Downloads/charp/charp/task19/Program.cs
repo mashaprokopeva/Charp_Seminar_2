5e835b621b903ead11625125fb180e02b1ce36be
Evgeny Norkin <norkinzheny@gmail.com>
2023-06-28 21:50:44 +0300

    first commit

---
+++ Downloads/charp/charp/task19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("ВВедите пятизначное число: ");
+string input = Console.ReadLine(); 
+if (input.Length != 5)
+{
+    System.Console.WriteLine("Вы ввели неправильное число!");
+}
+else if (input[0] == input[4] && input[1] == input[3])
+{
+    System.Console.WriteLine("Ваше число является палиндромом");
+}
+else
+{
+    System.Console.WriteLine("Ваше число не является палиндромом");
+}
+
+
+
+
+
+
+
+
---
+++ Downloads/charp/charp/task21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+//Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(firstPoint [0] - Sp [0],2 + Math.Pow (fp[1] - Span [1],2)))),3);
+
+ 
+        
+   Console.WriteLine("Введите координаты первой точки: ");
+        int[] firstPoint = Array.ConvertAll(Console.ReadLine()!.Split(" "), int.Parse);
+
+        Console.WriteLine("Введите координаты второй точки: ");
+        int[] secondPoint = Array.ConvertAll(Console.ReadLine()!.Split(" "), int.Parse);
+
+        double distance = CalculateDistance(firstPoint, secondPoint);
+        Console.WriteLine(Math.Round(distance, 2)); // округление после запятой на 2 знака
+    
+
+    static double CalculateDistance(int[] point1, int[] point2)
+    {
+        int deltaX = point2[0] - point1[0];
+        int deltaY = point2[1] - point1[1];
+        int deltaZ = point2[2] - point1[2];
+
+        double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+        return distance;
+    }
+
+     
